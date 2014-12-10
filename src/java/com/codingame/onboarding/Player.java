55b985bff13bf07295b6d1f862601ef3023b6323
Quentin Ecale <q.ecale@gmail.com>
2014-12-10 16:41:07 +0100

    change indent

--- src/java/com/codingame/onboarding/Player.java
+++ src/java/com/codingame/onboarding/Player.java
@@ -7,36 +7,36 @@ import java.math.*;
 public class Player {
 
 	public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 
-        // game loop
-        while (true) {            
-            System.out.println(getNearestEnemyName(in));
-        }
-    }
-	
+		// game loop
+		while (true) {
+			System.out.println(getNearestEnemyName(in));
+		}
+	}
+
 	public static String getNearestEnemyName(Scanner in) {
 		String nearestEnemyName = "";
 		int dist;
-		int distNearestEnemy=0;
-		 int count = in.nextInt(); // The number of current enemy ships within range
-         for (int i = 0; i < count; i++) {
-            String enemy = in.next();
+		int distNearestEnemy = 0;
+		int count = in.nextInt(); // The number of current enemy ships within
+		// range
+		for (int i = 0; i < count; i++) {
+			String enemy = in.next();
 			dist = in.nextInt();
-			if(i<=0) {
+			if (i <= 0) {
 				distNearestEnemy = dist;
 				nearestEnemyName = enemy;
-			}
-			else {
-				if(dist<distNearestEnemy) {
+			} else {
+				if (dist < distNearestEnemy) {
 					distNearestEnemy = dist;
 					nearestEnemyName = enemy;
 				}
 			}
-			
+
 		}
-		
+
 		return nearestEnemyName;
 	}
-	
+
 }
